2b17d8e1dda5fff1fccaa24efa518549e028b152
Samuel Trahan <Samuel.Trahan@noaa.gov>
2022-12-23 20:44:04 +0000

    faulty assumptions in bestlayout.py

--- bestlayout.py
+++ bestlayout.py
@@ -4,15 +4,15 @@ import sys
 
 def main():
     # Target number of gridpoints in each domain:
-    gridpoints=[601*601,
-                1201*1201]
+    gridpoints=[600*600,
+                1200*1200]
 
     # How close the number of gridpoints must be to those numbers in percent.
-    accuracy=5
+    accuracy=1.5
 
     # Allowed ranges of possible grid side lengths for each domain:
-    xyranges=[ [570,630],
-               [1140,1260] ]
+    xyranges=[ [580,620],
+               [1160,1240] ]
 
     print('must be within %.2f%% of target gridpoints: inner=%d outer=%d'%(accuracy,gridpoints[0],gridpoints[1]))
     
@@ -20,19 +20,24 @@ def main():
     nodes=45
 
     # Number of inner domain nodes.
-    # The ideal number is somewhere in the range of 14-16.
-    inner=[12,13,14,15,16,17,18,19,20,21]
+    # The ideal number is around 1/3 of the nodes
+    diff=range(-6,7)
+    mid=int(nodes*0.33333333)
+    inner=[ mid+x for x in diff ]
 
     # Number outer domain nodes:
     outer=[ nodes-i for i in inner]
     
     # Number of MPI ranks per node on fv3 compute nodes
     # Reasonable numbers are 32, 42, 60, 62, or 64. Anything else has no options for good performance.
-    ppn=32
+    ppn=42
 
-    #layout_algo = most_square_layout
+    # This seems to get the best performance:
     layout_algo = reversed_most_square_layout_that_integer_divides_ppn
-    
+
+    # Another option, but slightly worse performance:
+    #layout_algo = most_square_layout
+
     print('%d fv3 compute nodes with %d MPI ranks per node'%(nodes,ppn))
     print('inner domain node counts: '+str(inner))
     print('outer domain node counts: '+str(outer))
@@ -40,42 +45,41 @@ def main():
     
     for isize in range(len(inner)):
         answers=['','']
+        print('%d inner nodes, %d outer nodes ppn=%d:'%(inner[isize],outer[isize],ppn))
         for igrid in range(len(gridpoints)):
             gridrange=[ gridpoints[igrid]*(100.-accuracy)/100.,
                         gridpoints[igrid]*(100.+accuracy)/100. ]
             answers[igrid]=closest(gridrange,xyranges[igrid],inner[isize],ppn,'    ',gridpoints[igrid],igrid,layout_algo)
         if answers[0] and answers[1]:
-            print('%d inner nodes, %d outer nodes ppn=%d:'%(inner[isize],outer[isize],ppn))
-            sys.stdout.write('  Grid 0\n'+answers[0])
-            sys.stdout.write('  Grid 1\n'+answers[1])
+            sys.stdout.write('  Inner domain\n'+answers[0])
+            sys.stdout.write('  Outer domain\n'+answers[1])
+        else:
+            print('  No options.')
 
 def closest(gridrange,xyrange,nodes,ppn,indent,target,igrid,layout_algo):
     ppes=nodes*ppn
     answer = ''
     for xval in range(xyrange[0],xyrange[1]+1):
-        if igrid==0 and (xval-1)%3:
+        if igrid==0 and xval%3:
             continue
         for yval in range(xyrange[0],xyrange[1]+1):
-            if igrid==0 and (yval-1)%3:
+            if igrid==0 and yval%3:
                 continue
             xyval=xval*yval
             if not xyval%ppes and xyval<=gridrange[1] and xyval>=gridrange[0]:
                 layout = layout_algo(nodes,ppn,xval,yval)
-#                layout = most_square_layout(nodes,ppn,xval,yval)
-#                layout = reversed_most_square_layout_that_integer_divides_ppn(nodes,ppn,xval,yval)
                 badness = max(layout[0]/layout[1], layout[1]/layout[0])
                 if badness<2:
-                    answer += '%sgrid dims = %4d,%4d;  layout = %2d,%2d;  mpi patch = %.1f,%.1f;  gridpoints = %7d = %7.3f%% of target\n'%(
+                    answer += '%sgrid dims = %4d,%4d = %4d,%4d in input.nml;  layout = %2d,%2d;  mpi patch = %.1f,%.1f;  gridpoints = %7d = %7.3f%% of target\n'%(
                         indent,
                         xval,yval,
+                        xval+1,yval+1,
                         layout[0],layout[1],
                         xval/float(layout[0]),
                         yval/float(layout[1]),
                         xyval,
                         xyval/float(target)*100
                     )
-#                elif badness<10:
-#                    print('discard layout %d %d due to badness %f'%(layout[0],layout[1],badness))
     return answer
 
 def reversed_most_square_layout_that_integer_divides_ppn(nodes,ppn,nx,ny):
